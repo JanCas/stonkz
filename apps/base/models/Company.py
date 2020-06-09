7c1453b3f8dd979bcf8c867bce71794b0094e27a
Jan Cas <jan.l.cas@outlook.com>
2020-06-09 12:30:56 -0700

    update gitignore

--- apps/base/models/Company.py
+++ apps/base/models/Company.py
@@ -7,7 +7,6 @@ from apps.base.models.Ticker import Ticker
 class Company(Control):
     name = models.CharField(max_length=100, default=None, null=False)
     description = models.TextField(help_text='description of company', null=True)
-    earnings_last_quarter = models.FloatField(default=0, null=True)
     ticker_name = models.ForeignKey(Ticker, on_delete=models.CASCADE, null=True, blank=True, verbose_name='ticker')
     beta = models.FloatField(default=0, null=True)
     industry = models.CharField(max_length=20, default=None, null=True)
