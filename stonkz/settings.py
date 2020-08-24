02e681f12890cbdbfa44874b3cbc7f2148652ad5
Jan Cas <jan.l.cas@outlook.com>
2020-08-24 14:44:06 -0600

    Update settings.py

--- stonkz/settings.py
+++ stonkz/settings.py
@@ -121,11 +121,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Alpaca API
-ALPACA_API_KEY = 'PKKVWVRXK5AU5VLWDJN7'
-ALPACA_API_SECRET = 'sq0ujGL/d/8zAQWHBaTfoKsAS987hCur/HuGMZFH'
-ALPACA_API_BASE_URL = 'https://paper-api.alpaca.markets'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
